83ac51149acc886f07eef4d4270c519fe781b8f4
Hassan Abedi <hassan.abedi.t@gmail.com>
2015-04-28 21:36:33 +0430

    added documentaions to function definitions

--- snowballsampling.py
+++ snowballsampling.py
@@ -4,10 +4,13 @@ import copy
 from numpy.random import randint
 
 def randomseed(g):
+    """ this function recturns a single node from g, it's chosen with uniform probability """
     ux = randint(0,g.number_of_nodes(),1)
     return ux[0]
 
 def snowballsampling(g, seed, maxsize=50):
+    """ this function returns a set of nodes equal to maxsize from g that are collected from around seed node via
+        snownball sampling """
     if g.number_of_nodes() < maxsize:
         return [None]
     q = que.Queue()
@@ -25,6 +28,7 @@ def snowballsampling(g, seed, maxsize=50):
     return list(set(subgraph))
 
 def surroundings(g, subgraph):
+    """ this function returns the surrounding subgraph of input subgraph argument """ 
     surdngs = copy.copy(subgraph)
     for node in subgraph:
         for i in g.neighbors(node):
